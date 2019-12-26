e334c9bcb32b33ec6022243f8a521d4f41574e1a
Reuven Birnbaum <birnbaum.reuven@gmail.com>
2019-12-25 23:56:29 -0600

    improving twoSum

--- twoSum.py
+++ twoSum.py
@@ -3,19 +3,27 @@ from typing import List
 
 class twoSumSolution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        index=0;
+        complament = dict()
         size=len(nums)
-        stop=False 
-        for fir in range(size-1):
-            firNum = nums[fir]
-            for sec in range(fir+1,size):
-                if (firNum+nums[sec])==target:
-                    stop=True
-                    break
-            if stop:
-                break
-        if (not stop):
-            return [-1]
-        return [fir, sec]
+
+        while index<size:
+            #Check if each element is not in a complament dictionary:
+            # if not found ; put the elemnt in dictionary
+            # otherwise return list with complament and current index 
+            if target-nums[index] not in complament:
+                complament[nums[index]] = index
+                index+=1
+            else:
+                return [complament[target-nums[index]],index]
+    
+
+    
 
 class twoSumTestCase(unittest.TestCase):
 
@@ -25,10 +33,7 @@ class twoSumTestCase(unittest.TestCase):
         self.assertEqual(twoSums([1,2,3],4),[0,2])
         self.assertEqual(twoSums([1,2,3],5),[1,2])
         self.assertEqual(twoSums([3,2,4],6),[1,2])
-
-    def testFalseTwoSums(self):
-        twoSums = twoSumSolution().twoSum       
-        self.assertEqual(twoSums([1,2,3],0),[-1])
-
+        self.assertEqual(twoSums([2,7,11,15],9),[0,1])
+                
 if __name__ == '__main__':
     unittest.main()
