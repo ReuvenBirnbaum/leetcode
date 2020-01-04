610f542c763d4a55097f8d972995c66cf66bde62
Reuven Birnbaum <birnbaum.reuven@gmail.com>
2020-01-03 23:43:31 -0600

    small optimization O(2n)->O(n)

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -2,19 +2,32 @@ import unittest
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        Finds the length of the longest substring of the given string without 
+        repeating characters
+        :type s: str
+        :rtype int
+        """
         size =len(s)
         i,j,ans= 0,0,0
         found = dict()
+        # Iterate through array with a window defined by [i,j)  
         while j<size:
+            # If j^th character not found redefine max if neccesary 
             if (s[j] not in found):
-              found[s[j]] =j
-              j+=1
-              ans = max(j-i,ans)
+                ans = max(j-i+1,ans)
+            # Move i^th index to the next found character while intermediate
+            # found elements
             else:
-                found.pop(s[i])
-                i+=1
+                iNew = found[s[j]]+1
+                while i<iNew:
+                    found.pop(s[i])
+                    i+=1
                 if size-i<ans:
                     break
+            # Insert the found character and inrement j
+            found[s[j]]=j
+            j+=1
         return ans
             
 
@@ -23,10 +36,13 @@ class lengthTestCase(unittest.TestCase):
 
     def testBasiclength(self):
         lLSS = Solution().lengthOfLongestSubstring
+        self.assertEqual(lLSS("wobgrovw"),6)
         self.assertEqual(lLSS("bbbbb"),1)
         self.assertEqual(lLSS("pwwkew"),3)
         self.assertEqual(lLSS("abcabcbb"),3)
+        self.assertEqual(lLSS("tmmzuxt"),5)
         
+
                 
 if __name__ == '__main__':
     unittest.main()
